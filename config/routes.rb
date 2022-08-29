ba3ac137984fac51ec6f65393c76b54180e4e0d5
drlocon <prirautem.srw.09@gmail.com>
2022-08-29 09:16:58 +0000

    [Add]コントーラとビューを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,34 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'groups/new'
+    get 'groups/index'
+    get 'groups/show'
+    get 'groups/edit'
+  end
+  namespace :public do
+    get 'chats/show'
+  end
+  namespace :public do
+    get 'likes/new'
+    get 'likes/index'
+    get 'likes/show'
+    get 'likes/edit'
+  end
+  namespace :public do
+    get 'users/index'
+    get 'users/show'
+    get 'users/edit'
+  end
+  namespace :public do
+    get 'homes/top'
+  end
+  namespace :admin do
+    get 'users/show'
+    get 'users/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
   # devise/adminサイド
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
